a71fb25e7955f0c15d47ea471d82a08b0e13b63e
jaybenaim <benaimjacob@gmail.com>
2020-09-03 19:13:05 -0400

    fixed app.test to test agains initial render

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,42 @@
-﻿import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+﻿import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
+import unregister from "./registerServiceWorker";
+import ErrorBoundary from "./components/ErrorBoundary";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import { createFirestoreInstance } from "redux-firestore";
+
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./config/firebase";
+import firebase from "firebase/app";
+
+const rrfConfig = {
+  userProfile: "users",
+  useFirestoreForProfile: true,
+};
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance, //since we are using Firestore
+};
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>, 
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <HashRouter basename="VirtualLearning">
+          <ErrorBoundary>
+            <App />
+          </ErrorBoundary>
+        </HashRouter>
+      </ReactReduxFirebaseProvider>
+    </Provider>,
     div
   );
 });
