c6c27229c00ac48b33b3e9946b5e43e3472c57c8
jaybenaim <benaimjacob@gmail.com>
2020-09-03 16:47:26 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+React firebase app with auth. 
+Ready for gh-pages deploy. 
+
+
+1. Clone project onto your local computer.
+
+   git clone https://github.com/jaybenaim/react-starter-template
+   
+2. Remove git folder and re-initialize
+   rm -rf .git;git init; git add .;git push origin master
+
+2. Install dependencies. 
+
+   npm i 
+
+3. Add .env variables for firebase config 
+
+4. Deploy app to gh-pages. 
+   
+   npm run deploy
+   
+5. Add homepage to package.json
+
+   "homepage" : "https://github.com/jaybenaim/react-starter-template"
+   
+See the demo at https://github.com/jaybenaim/react-starter-template
---
+++ build/precache-manifest.cde701403d061cfa681ab7a2a3cc8869.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be661f137b8616d4aa46fadd3e5a2332",
+    "url": "/react-starter-template/index.html"
+  },
+  {
+    "revision": "6491ae5118d43b9278bc",
+    "url": "/react-starter-template/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "056d361a62703cb98f30",
+    "url": "/react-starter-template/static/css/main.f8e44577.chunk.css"
+  },
+  {
+    "revision": "6491ae5118d43b9278bc",
+    "url": "/react-starter-template/static/js/2.d07261c1.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/react-starter-template/static/js/2.d07261c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056d361a62703cb98f30",
+    "url": "/react-starter-template/static/js/main.c3a0e4b8.chunk.js"
+  },
+  {
+    "revision": "495a17ee549bc779519d",
+    "url": "/react-starter-template/static/js/runtime-main.39979ba4.js"
+  }
+]);
