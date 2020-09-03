f34806fe875f2a4bd1bba78eaa9b9046ccb13027
jaybenaim <benaimjacob@gmail.com>
2020-09-03 16:49:02 -0400

    Updates

---
+++ precache-manifest.8fcf8c9cde8e61cc7a5c833b9ad2e372.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc2530f2d97845503bc9ca6abbdb53a",
+    "url": "/react-starter-template/index.html"
+  },
+  {
+    "revision": "54838b2ba93272c670a7",
+    "url": "/react-starter-template/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "40e8b6eb7751375893bc",
+    "url": "/react-starter-template/static/css/main.f8e44577.chunk.css"
+  },
+  {
+    "revision": "54838b2ba93272c670a7",
+    "url": "/react-starter-template/static/js/2.48934e0f.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/react-starter-template/static/js/2.48934e0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40e8b6eb7751375893bc",
+    "url": "/react-starter-template/static/js/main.61974157.chunk.js"
+  },
+  {
+    "revision": "495a17ee549bc779519d",
+    "url": "/react-starter-template/static/js/runtime-main.39979ba4.js"
+  }
+]);
